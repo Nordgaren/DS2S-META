e781e8155980fa2c28e564392f6e4d4513ea2de3
pseudostripy <41550973+pseudostripy@users.noreply.github.com>
2022-06-12 16:18:14 +0100

    Add test character implementation

--- DS2S META/TabControls/StatsControl.xaml.cs
+++ DS2S META/TabControls/StatsControl.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Xceed.Wpf.Toolkit;
 
 namespace DS2S_META
 {
@@ -21,6 +22,9 @@ namespace DS2S_META
     /// </summary>
     public partial class StatsControl : METAControl
     {
+        public List<IntegerUpDown> nudLevels => new List<IntegerUpDown>() 
+            { nudVig, nudEnd, nudVit, nudAtt, nudStr, nudDex, nudAdp, nudInt, nudFth };
+
         public StatsControl()
         {
             InitializeComponent();
@@ -101,33 +105,143 @@ namespace DS2S_META
 
         private void Reset_Click(object sender, RoutedEventArgs e)
         {
-            nudVig.Value = nudVig.Minimum;
-            nudEnd.Value = nudEnd.Minimum;
-            nudVit.Value = nudVit.Minimum;
-            nudAtt.Value = nudAtt.Minimum;
-            nudStr.Value = nudStr.Minimum;
-            nudDex.Value = nudDex.Minimum;
-            nudAdp.Value = nudAdp.Minimum;
-            nudInt.Value = nudInt.Minimum;
-            nudFth.Value = nudFth.Minimum;
+            foreach (IntegerUpDown nudLev in nudLevels)
+                nudLev.Value = nudLev.Minimum;
         }
 
         private void Max_Click(object sender, RoutedEventArgs e)
         {
-            nudVig.Value = 99;
-            nudEnd.Value = 99;
-            nudVit.Value = 99;
-            nudAtt.Value = 99;
-            nudStr.Value = 99;
-            nudDex.Value = 99;
-            nudAdp.Value = 99;
-            nudInt.Value = 99;
-            nudFth.Value = 99;
+            foreach (IntegerUpDown nudLev in nudLevels)
+                nudLev.Value = 99;
         }
 
         private void RestoreHumanity_Click(object sender, RoutedEventArgs e)
         {
             Hook.ApplySpecialEffect(100000010);
+        }
+
+        private void NewTestCharacter_Click(object sender, RoutedEventArgs e)
+        {
+            // Define character multi-items
+            var lifegemid = 60010000;
+            var oldradid = 60030000;
+            var mushroomid = 60035000;
+            var blessingid = 60105000;
+            var effigyid = 60151000;
+            var mossid = 60070000;
+            var wiltherbid = 60060000;
+            var oozeid = 60240000;
+            var gprid = 60250000;
+            var dprid = 60270000;
+            var featherid = 60355000;
+            var branchid = 60537000;
+            var witchurnid = 60550000;
+            var firebombid = 60570000;
+            var blkfirebombid = 60575000;
+            var dungid = 60595000;
+            var poisonknifeid = 60590000;
+            var greatheroid = 60720000;
+            var odosid = 64320000;
+            var skullsid = 60530000;
+            var torchid = 60420000;
+            //
+            var titshardid = 60970000;
+            var ltsid = 60975000;
+            var chunkid = 60980000;
+            var slabid = 60990000;
+            var twinklingid = 61000000;
+            var ptbid = 61030000;
+            var boltstoneid = 61070000;
+            var darkstoneid = 61090000;
+            var rawstoneid = 61130000;
+            var palestoneid = 61160000;
+
+            var multi_items = new List<int>() { lifegemid, oldradid, mushroomid, blessingid, effigyid, mossid, wiltherbid,
+                                                oozeid, gprid, dprid, featherid, branchid, witchurnid, firebombid, blkfirebombid,
+                                                dungid, poisonknifeid, greatheroid, odosid, skullsid, torchid,
+                                                titshardid, ltsid, chunkid, slabid, twinklingid, ptbid, boltstoneid, darkstoneid,
+                                                rawstoneid, palestoneid};
+            foreach (int id in multi_items)
+                Hook.GiveItemSilently(id, 95, 0, 0);
+
+            // ammo
+            var woodarrowid = 60760000;
+            var ironarrowid = 60770000;
+            var magicarrowid = 60780000;
+            var firearrowid = 60800000;
+            var psnarrowid = 60820000;
+            var heavyboltid = 60920000;
+
+            var ammo_items = new List<int>() { woodarrowid, ironarrowid, magicarrowid, firearrowid, psnarrowid, heavyboltid };
+            foreach (int id in ammo_items)
+                Hook.GiveItemSilently(id, 950, 0, 0);
+
+            // one-upgrade stuff:
+            var estusid = 60155000;
+            var binoid = 6100000;
+            var bucklerid = 11000000;
+            var goldenshield = 11050000;
+            var ironparmaid = 11020000;
+
+            var clo1id = 40020001;
+            var bladesid = 40160000;
+            var catringid = 40420000;
+            var soul1ringid = 40370001;
+            var flynnsid = 41100000;
+
+            var staffid = 3800000;
+            var chimeid = 4010000;
+            var pyroid = 5400000;
+            var dwid = 34060000;
+            var slbid = 32260000;
+
+            var bflyskirtid = 21470103;
+            var bflywingsid = 21470101;
+            var tseldorahatid = 22460100;
+            var tseldorabodyid = 22460101;
+            var tseldoraglovesid = 22460102;
+            var tseldorapantsid = 22460103;
+
+            var single_items = new List<int>() { estusid, binoid, bucklerid, goldenshield, ironparmaid,
+                                                clo1id, bladesid, catringid, soul1ringid, flynnsid,
+                                                staffid, chimeid, pyroid, dwid, slbid,
+                                                bflyskirtid, bflywingsid, tseldorahatid, tseldorabodyid, tseldoraglovesid, tseldorapantsid};
+            foreach (int id in single_items)
+                Hook.GiveItemSilently(id, 1, 0, 0);
+
+            // upgraded speedrun weapons:
+            var daggerid = 1000000;
+            var rapierid = 1500000;
+            var mace = 2410000;
+            var shortbowid = 4200000;
+            var lightcbwid = 4600000;
+            var uchiid = 1700000;
+
+            var upgr_weapons = new List<int>() { daggerid, rapierid, mace, shortbowid, lightcbwid, uchiid };
+            foreach (int id in upgr_weapons)
+                Hook.GiveItemSilently(id, 1, 10, 0);
+
+            // Misc others:
+            Hook.GiveItemSilently(rapierid, 1, 0, 0);   // basic rapier
+            Hook.GiveItemSilently(rapierid, 1, 10, 3);  // lightning rapier
+            Hook.GiveItemSilently(rapierid, 1, 10, 4);  // dark rapier
+            var ritbid = 5350000;
+            Hook.GiveItemSilently(ritbid, 1, 10, 3);    // lightning RITB
+            var decapitateid = 63017000; // :D
+            Hook.GetItem(decapitateid, 1, 0, 0);        // show visibly
+
+
+            // Used to create a character with commonly useful things
+            RestoreHumanity_Click(sender, e);   // human char
+            Max_Click(sender, e);               // max levels
+            Hook.UnlockBonfires();              // unlock all bonfire
+
+
+            // to tidy:
+            DS2SBonfire majula = new DS2SBonfire(168034304, 4650, "The Far Fire");
+            Hook.LastBonfireID = majula.ID;
+            Hook.LastBonfireAreaID = majula.AreaID;
+            Hook.Warp(majula.ID);
         }
     }
 }
